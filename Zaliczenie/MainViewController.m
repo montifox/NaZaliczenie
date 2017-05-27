0ae0ff4a5d9fa94365070deb95d7256d7d8f6287
Mateusz Chojnacki <chojnackimateusz95@wp.pl>
2017-05-27 18:31:40 +0200

    start create interface file

--- Zaliczenie/MainViewController.m
+++ Zaliczenie/MainViewController.m
@@ -14,9 +14,14 @@
 
 @implementation MainViewController
 
+@synthesize arrayNUMB, arrayCOUNT, costARRAY, dateARRAY, itemARRAY;
+
+
+
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
+    
+    
 }
 
 - (void)didReceiveMemoryWarning {
@@ -24,15 +29,7 @@
     // Dispose of any resources that can be recreated.
 }
 
-/*
-#pragma mark - Navigation
 
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
-}
-*/
 
 - (IBAction)leftBTN:(id)sender {
 }
