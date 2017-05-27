704e8016089cc20305db6a6cc3c38ef8d2cdc05b
Mateusz Chojnacki <chojnackimateusz95@wp.pl>
2017-05-27 18:12:43 +0200

    Initial Commit

---
+++ Zaliczenie/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Zaliczenie
+//
+//  Created by Mateusz Chojnacki on 27.05.2017.
+//  Copyright © 2017 Mateusz Chojnacki. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Zaliczenie/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Zaliczenie
+//
+//  Created by Mateusz Chojnacki on 27.05.2017.
+//  Copyright © 2017 Mateusz Chojnacki. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
